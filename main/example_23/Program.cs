ca76f848ed742fc5314b36967600ad539fad1b07
unknown <Ivan>
2023-04-17 16:08:56 +0700

    done example_23

--- main/example_23/Program.cs
+++ main/example_23/Program.cs
@@ -6,3 +6,21 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+Console.Write("Введите число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int TableOfCubes(int num){
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index}  {index*index*index}");
+        index++;
+    }
+    return index;
+}
+int tableOfCubes = TableOfCubes(number);
+Console.WriteLine($"{tableOfCubes}");
+
+// Если делать через void, то тогда дополнительной цифры в конце нету. Но в моем варианте выходит что добавляется 
+// еще одно число index. Мог написать правильно, но хочу разобраться в чем дело.
